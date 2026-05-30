548041fc51d2c212c3c9a658f9ef259abe4f0d38
Jonas Christian Larsen <jonaschristianlarsen@gmail.com>
2026-05-30 18:17:15 +0200

    31 - kommentar

--- for loop/31 - Tabeltæller/Program.cs
+++ for loop/31 - Tabeltæller/Program.cs
@@ -21,11 +21,11 @@ namespace _31___Tabeltæller
             //Overskrift
             Console.WriteLine(" \x1b[4m33 tabellen\x1b[0m\n"); //x1b[4 starter understregning af skrift, x1b[0m stopper understregningen
 
-            //Løkke
-            //Gentager alt mellem {} indsat på linjen under for, indtil dens værdi overskrider stop værdien.
-            for (int talMultiplikation = 1;/*variabel til løkken oprettes med værdien 1*/ talMultiplikation <= 20;/*løkken skal gentage sig selv så længe variablen er under eller lig med 20, 33 tabllen forsætter til 20 * 33*/ talMultiplikation++)/*variablen stiger med 1 hver gang løkken er blevet kørt*/
+            //For-løkke
+            //Gentager alt mellem {} indsat på linjen under for (), indtil den brugte variabels værdi overskrider, stop værdien.
+            for (int talMultiplikation = 1;/*variabel til løkken oprettes med værdien 1*/ talMultiplikation <= 20;/*løkken skal gentage sig selv så længe variablen er under eller lig med 20, her betyder det at 33 tabllen forsætter til 20 * 33*/ talMultiplikation++)/*variablen stiger med 1 hver gang løkken er blevet kørt igennem*/
             {
-                Console.WriteLine($"{talMultiplikation, 2} * {tabel33, 2} = {talMultiplikation * tabel33, 3}"); //løkkens variabels værdi ganges med tabellens værdi.
+                Console.WriteLine($"{talMultiplikation, 2} * {tabel33, 2} = {talMultiplikation * tabel33, 3}"); //løkkens variabels værdi ganges med tabellens værdi, på den opnås det rigtige tal til leddet i tabellen.
             }                                                                                                   //Med et komma efter en variabel inde i {} kan man fastsætte, at den altid fylder et bestemt antal tegn. Sådan kommer tallene til at stå under hinanden selvom de går fra at være encifrede til flerecifrede.
             Console.ReadKey();
         }
