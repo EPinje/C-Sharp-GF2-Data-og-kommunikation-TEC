e2d8aeeb5ae9bfd08479fd23eebc36081e166d52
Jonas Christian Larsen <jonaschristianlarsen@gmail.com>
2026-05-30 17:51:56 +0200

    Mindre rettlese til aflevering

--- for loop/31 - Tabeltæller/Program.cs
+++ for loop/31 - Tabeltæller/Program.cs
@@ -10,6 +10,11 @@ namespace _31___Tabeltæller
     {
         static void Main(string[] args)
         {
+            //Jonas Christian Larsen
+            //Opgave 31 - Tabeltæller
+            //30.05.2026
+
+
             //den valgte tabel
             int tabel33 = 33;
 
@@ -17,10 +22,11 @@ namespace _31___Tabeltæller
             Console.WriteLine(" \x1b[4m33 tabellen\x1b[0m\n"); //x1b[4 starter understregning af skrift, x1b[0m stopper understregningen
 
             //Løkke
+            //Gentager alt mellem {} indsat på linjen under for, indtil dens værdi overskrider stop værdien.
             for (int talMultiplikation = 1;/*variabel til løkken oprettes med værdien 1*/ talMultiplikation <= 20;/*løkken skal gentage sig selv så længe variablen er under eller lig med 20, 33 tabllen forsætter til 20 * 33*/ talMultiplikation++)/*variablen stiger med 1 hver gang løkken er blevet kørt*/
             {
                 Console.WriteLine($"{talMultiplikation, 2} * {tabel33, 2} = {talMultiplikation * tabel33, 3}"); //løkkens variabels værdi ganges med tabellens værdi.
-            }                                                                                                   //Med et komma efter variablen kan man fastsætte at den altid fylder det næste tals antal tegn. Sådan kommer tallene til at stå under hinanden selvom de går fra at være encifrede til flere cifre.
+            }                                                                                                   //Med et komma efter en variabel inde i {} kan man fastsætte, at den altid fylder et bestemt antal tegn. Sådan kommer tallene til at stå under hinanden selvom de går fra at være encifrede til flerecifrede.
             Console.ReadKey();
         }
     }
