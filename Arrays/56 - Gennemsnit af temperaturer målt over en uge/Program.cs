302fc57b7ae64518f87d970883d5980fd8a841ac
Jonas Christian Larsen <jonaschristianlarsen@gmail.com>
2026-06-10 21:02:04 +0200

    56 - Færdig

--- Arrays/56 - Gennemsnit af temperaturer målt over en uge/Program.cs
+++ Arrays/56 - Gennemsnit af temperaturer målt over en uge/Program.cs
@@ -14,33 +14,109 @@ namespace _56___Gennemsnit_af_temperaturer_målt_over_en_uge
             //56 - Gennemsnit af temperaturer målt over en uge
             //09.06.2026
 
-            //Programmet beder brugeren om input for, hvad temperaturen har været fra mandag til søndag. Derefter gengiver det dagenes temperatur og slutter af med at oplyse gennemsnitstemperaturen for alle 7 dage.
+            Console.OutputEncoding = Encoding.UTF8; //Specialtegn som æ, ø og å, kan bruges i programmet
 
-            //variabel til at udregne gennemsnit af temperaturerne
-            double sum = 0;
+            bool loop = true;
 
-            //Arrays til at gengive ugedage og indhente temperaturer
-            string[] ugedag = {"Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fradag", "Lørdag", "Søndag"};
-            double[] temp = new double[7]; //temp er givet 7 pladser. En for hver ugedag
-
-            //for-løkke, som beder om og tager imod input fra brugeren
-            for (int i = 0; i <= 6; i++)
+            //loop
+            do //do gentager alt indenfor dens {} så længe loop = true i while
             {
-                Console.Write($"Input gennemsnitstemperaturen for {ugedag[i].ToLower()}: "); //ugedag[i] vælger ugedag fra det definerede array og inputter dem i kronologisk rækkefølge i takt med at i's værdi stiger. .ToLower() sørger for at ugedagene står med småt, da de er defineret med stort begyndelsesbogstav.
-                temp[i] = Convert.ToDouble(Console.ReadLine()); //input fra brugeren gemmes i arrayet temp
-                sum = sum + temp[i]; //sum bliver tillagt temperaturen for hver ugedag
+                Console.Clear(); //renser skærmen for tekst
+                
+                //Info om programmet
+                Console.WriteLine("Programmet beder brugeren om input for, hvad temperaturen har været fra mandag til søndag.\n\nDerefter gengiver det dagenes temperaturer og slutter af med at oplyse gennemsnitstemperaturen for alle 7 dage.");
+                Console.WriteLine("\nVil du have temperaturen til at stå i:");
+                Console.WriteLine("\n1. Grader Celcius");
+                Console.WriteLine("2. Fahrenheit");
+                Console.Write("\nIndtast 1 eller 2: ");
+
+                //Input fra bruger
+                string input = Console.ReadLine();
+
+                Console.Clear();
+
+                double sum = 0; //variabel til at udregne gennemsnit af temperaturerne
+
+                //Arrays til at gengive ugedage og indhente temperaturer
+                string[] ugedag = {"Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fradag", "Lørdag", "Søndag"};
+                double[] temp = new double[7]; //temp er givet 7 pladser. En for hver ugedag
+                bool inputCheck = true;
+
+                switch (input) //switch vælger hvilken kode, der er relevant for brugeren
+                {
+                    //case til grader Celsius
+                    case ("1"):
+
+                         for (int i = 0; i <= 6; i++) //for-løkke, som beder om og tager imod input fra brugeren
+                         {
+                             do
+                             {
+                                 Console.Write($"Input temperaturen for {ugedag[i].ToLower()} i °C: "); //ugedag[i] vælger ugedag fra det definerede array og inputter dem i kronologisk rækkefølge i takt med at i's værdi stiger. .ToLower() sørger for at ugedagene står med småt, da de er defineret med stort begyndelsesbogstav
+                                 input = Console.ReadLine(); //brugerens input gemmes i input
+                                 inputCheck = double.TryParse(input, out temp[i]);//double TryParse tjekker om brugerens input er et tal. Hvis det er gemmes det som double i temp arrayet. For hver omgang af for () gemmes en ny variabel i temp
+                                 sum = sum + temp[i]; //sum bliver tillagt temperaturen for hver ugedag
+
+                                //info ved forkert input
+                                if (!inputCheck)
+                                    Console.WriteLine("\nTemperaturen skal angives med tal. Prøv igen.\n");
+                             }
+                             while (!inputCheck); //hvis inputtet ikke er double, gentages do
+                         }
+
+                Console.Clear();
+
+                        for (int i = 0; i <= 6; i++) //løkken gengiver de indtastede værdier i en udskrift, hvor de to arryas arbejder sammen, så hver temperatur står ud for den korrekte ugedag
+                        {
+                            Console.WriteLine($"{ugedag[i]} var temperaturen {temp[i]} °C"); //temp[i] putter de gemte værdier fra brugerens input ind
+                        }
+
+                        Console.Write($"\nGennemsnitstemperaturen i løbet af ugen var {sum / 7:N2} °C"); //summen divideres med 7 for at få gennemsnittet for alle dagene
+                        Console.ReadKey();
+
+                        loop = false; //når loop er false afsluttes do-while (programmet)
+
+                        break; //casen endes her
+
+                    //case til Fahrenheit
+                    case ("2"): //en kopi af den første case med C i udskrift ændret til F
+
+                        for (int i = 0; i <= 6; i++)
+                        {
+                            do
+                            {
+                                Console.Write($"Input temperaturen for {ugedag[i].ToLower()} i °C: ");
+                                input = Console.ReadLine();
+                                inputCheck = double.TryParse(input, out temp[i]);
+                                sum = sum + temp[i];
+
+                                if (!inputCheck)
+                                    Console.WriteLine("\nTemperaturen skal angives med tal. Prøv igen.\n");
+                            }
+                            while (!inputCheck);
+                        }
+
+                        Console.Clear();
+
+                        for (int i = 0; i <= 6; i++)
+                        {
+                            Console.WriteLine($"{ugedag[i]} var temperaturen {temp[i]} °F");
+                        }
+
+                        Console.Write($"\nGennemsnitstemperaturen i løbet af ugen var {sum / 7:N2} °F");
+                        Console.ReadKey();
+
+                        loop = false;
+
+                        break;
+
+                    //Sikkerhedsnet
+                    default: //hvis brugerens input ikke passer til en case ledes de tilbage til startmenuen
+                        Console.Write("Du skal indtaste 1 eller 2 for at fortsætte.\n\nTryk på en tast for at gå tilbage.");
+                        Console.ReadLine();
+                        break;
+                }
             }
-
-            Console.WriteLine(); //linjeskift for bedre layout
-
-            //løkken gengiver de indtastede værdier i en udskrift, hvor de to arryas arbejder sammen, så hver temperatur står ud for den korrekte ugedag
-            for (int i = 0; i <= 6; i++)
-            {
-                Console.WriteLine($"{ugedag[i]} var gennemsnitstemperaturen {temp[i]} grader"); //temp[i] putter de gemte værdier fra brugerens input ind
-            }
-
-            Console.WriteLine($"\nGennemsnitstemperaturen i løbet af hele ugen var {sum / 7:N2} grader"); //summen divideres med 7 for at få gennemsnittet for alle dagene
-            Console.ReadKey();
+            while (loop);
         }
     }
 }
--- Switch Case/46 - Billetsalg for svømmehal/Program.cs
+++ Switch Case/46 - Billetsalg for svømmehal/Program.cs
@@ -14,7 +14,7 @@ namespace _46___Billetsalg_for_svømmehal
             //46 - Billetsalg for svømmehal
             //09.06.2026
 
-            string o = "";
+            string tomString = "";
 
             string[] billetType = new string[]
             {   "Voksne",
@@ -28,11 +28,11 @@ namespace _46___Billetsalg_for_svømmehal
             };
 
             double[] priser = new double[] {42, 23, 0, 15, 10, 330, 175, 135};
-
             int[] stk = new int[8];
+            double[] priserxstk = new double[8];
+            double total = 0;
 
-
-            Console.WriteLine($"\n{o,35}Velkommen til Ballerup Svømmehal");
+            Console.WriteLine($"\n{tomString,35}Velkommen til Ballerup Svømmehal");
 
             Console.WriteLine("\n\nBrug tallene til venstre og tryk enter for at navigere fundt i menuerne");
             Console.WriteLine("Hvis du befinder dig i en af de indre menuer kan du bruge alle andre input til at gå tilbage til den forrige menu");
@@ -41,8 +41,6 @@ namespace _46___Billetsalg_for_svømmehal
             Console.WriteLine("\n\n1. Billetbestilling");
             Console.Write("\n\nTryk enter for at bekræfte dine input: ");
             string input = Console.ReadLine();
-
-            Console.Clear();
 
             bool loop = true;
 
@@ -54,72 +52,118 @@ namespace _46___Billetsalg_for_svømmehal
 
                     do
                     {
+                        Console.Write("\x1b[3J");
+                        Console.Clear();
+
                         Console.WriteLine($"Hvilken type billet ønsker du at købe?\n");
 
                         for (int i = 0; i <= 7; i++)
                         {
-                            Console.WriteLine($"{i + 1}. {billetType[i]}\n{o,3}Pris: {priser[i]} dkk\n");
+                            Console.WriteLine($"{i + 1}. {billetType[i]}\n{tomString,3}Pris: {priser[i]} dkk\n");
                         }
                             Console.WriteLine("9. Betaling");
 
                             Console.WriteLine($"\nHvis du bestiller den samme type billet to gange overskrides den ældste bestilling af den nyeste!\n");
                             input = Console.ReadLine();
 
+                            Console.Write("\x1b[3J");
                             Console.Clear();
 
-                        switch (input)
+                            switch (input)
                         {
                             case ("1"):
                                 
-                                Console.WriteLine($"Hvor mange stk. skal du bruge?\n");
+                                Console.Write($"Hvor mange stk. skal du bruge? ");
                                 stk[0] = Convert.ToInt32(Console.ReadLine());
                                 break;
 
                             case ("2"):
 
-                                Console.WriteLine($"Hvor mange stk. skal du bruge?\n");
+                                Console.Write($"Hvor mange stk. skal du bruge? ");
                                 stk[1] = Convert.ToInt32(Console.ReadLine());
                                 break;
 
                             case ("3"):
 
-                                Console.WriteLine($"Hvor mange stk. skal du bruge?\n");
+                                Console.Write($"Hvor mange stk. skal du bruge? ");
                                 stk[2] = Convert.ToInt32(Console.ReadLine());
                                 break;
 
                             case ("4"):
 
-                                Console.WriteLine($"Hvor mange stk. skal du bruge?\n");
+                                Console.Write($"Hvor mange stk. skal du bruge? ");
                                 stk[3] = Convert.ToInt32(Console.ReadLine());
                                 break;
 
                             case ("5"):
 
-                                Console.WriteLine($"Hvor mange stk. skal du bruge?\n");
+                                Console.Write($"Hvor mange stk. skal du bruge? ");
                                 stk[4] = Convert.ToInt32(Console.ReadLine());
                                 break;
 
                             case ("6"):
 
-                                Console.WriteLine($"Hvor mange stk. skal du bruge?\n");
+                                Console.Write($"Hvor mange stk. skal du bruge? ");
                                 stk[5] = Convert.ToInt32(Console.ReadLine());
                                 break;
 
                             case ("7"):
 
-                                Console.WriteLine($"Hvor mange stk. skal du bruge?\n");
+                                Console.Write($"Hvor mange stk. skal du bruge? ");
                                 stk[6] = Convert.ToInt32(Console.ReadLine());
                                 break;
 
                             case ("8"):
 
-                                Console.WriteLine($"Hvor mange stk. skal du bruge?\n");
+                                Console.Write($"Hvor mange stk. skal du bruge? ");
                                 stk[7] = Convert.ToInt32(Console.ReadLine());
                                 break;
 
                             case ("9"):
 
-                                break;
+                                for (int i = 0; i <= 7; i++)
+                                    {
+                                        priserxstk[i] = stk[i] * priser[i];
+                                        total = total + priserxstk[i];
+                                    }
+
+                                Console.WriteLine("Vil du bekræfte købet af:\n");
+
+                                for (int i = 0; i <= 7; i++)
+                                    {
+                                        if (stk[i] > 0)
+                                        Console.WriteLine($"Billettype: {billetType[i]}\nStk.: {stk[i]}\nPris: {priserxstk[i]} dkk\n");
+                                    }
+
+                                    Console.WriteLine($"Samlet pris: {total} dkk");
+
+                                    Console.Write("j/n? ");
+                                    input = Console.ReadLine();
+
+                                    switch (input)
+                                    {
+                                        case ("j"):
+                                            Console.WriteLine($"{tomString, 15}Kvittering:\n");
+
+                                            for (int i = 0; i <= 7; i++)
+                                            {
+                                                if (stk[i] > 0)
+                                                    Console.WriteLine($"Billettype: {billetType[i]}\nStk.: {stk[i]}\nPris: {priserxstk[i]} dkk\n");
+                                            }
+
+                                            Console.WriteLine($"Samlet pris: {total} dkk");
+                                            break;
+
+                                        case ("n"):
+                                            break;
+                                    }
+                                         
+
+                                    Console.ReadKey();
+
+
+
+                                    break;
 
                             default:
                                 
@@ -128,6 +172,8 @@ namespace _46___Billetsalg_for_svømmehal
 
                     }
                     while (loop);
+
+                    break;
 
                     default:
                         loop = false;
